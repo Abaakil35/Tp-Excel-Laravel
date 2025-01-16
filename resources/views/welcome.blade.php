585333574703aa361dfe67c1678ee4d7c2913bf9
Abaakil-Ayoub <154565344+Abaakil35@users.noreply.github.com>
2025-01-16 17:56:15 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Note App</title>
 </head>
+<style>
+    body {
+        background-color: #f1f1f1;
+        font-family: Arial, sans-serif;
+    }
+    h1 {
+        text-align: center;
+        margin-top: 50px;
+    }
+    .container {
+        display: flex;
+        justify-content: center;
+        margin-top: 50px;
+    }
+    button {
+        padding: 10px 20px;
+        background-color: #4CAF50;
+        color: white;
+        border: none;
+        cursor: pointer;
+        border-radius: 5px;
+    }
+    button:hover {
+        background-color: #45a049;
+    }
+</style>
 <body>
-    <h1> Hello World</h1>
-    
+  
+<h1>Hello in Excel Reader</h1>
+<div class="container">
+    <a href="/upload">
+        <button>Begin</button>
+    </a>
+</div>
 </body>
 </html>
